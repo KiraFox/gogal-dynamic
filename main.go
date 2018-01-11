6b108ed3910ce148482934d60a8cb8c75454ba9f
Kira Fox <kira.fox@foxypossibilities.com>
2018-01-11 13:38:53 -0500

    Chapter 3.1 Routing using if/else statements

--- main.go
+++ main.go
@@ -6,25 +6,44 @@ import (
 )
 
 /*
-This is the function that we use to process incoming web request
+This is the function that we use to process incoming web request: routing
+using If/Else statements
 
--- Every time someone visits your website, the code in handlerFunc(...) gets
-run and determines what to return to the visitor
--- We will have different handlers for when users visit different pages on our
-application
--- All handlers take the same two arguments. An http.ResponseWriter declared as
-w in our current code, and a pointer to an http.Request, declared as r in our
-current code.
--- http.ResponseWriter : This is a structure that allows us to modify the
-response that we want to send to whoever visited our website. Default
-implements the Write() method that allows us to write to the response body. Has
-methods that help us set headers when we need to
--- *http.Request : This is a structure used to access data from the web
-request. For example, we might use this to get the users email address and
-password after they sign up for our web application.
+-- r.URL.Path == "/"  : First we get the URL from the Request object (r), which
+returns a *url.URL. This struct has a field on it named Path that returns the
+path of a URL. For example, if the URL was http://lenslocked.com/docs/abc then
+the Path would be /docs/abc. The only exception here is that an empty path is
+always set to "/"" , or the root path (home page).
+-- Once we have the path, we use it to determine what page to render (show the
+user). When the user is visiting the root path (/) we return our Welcome page.
+If the user is visiting our contact page (/contact) we return a page with
+information on how to contact.
+-- If neither of these criteria are met, we write the 404 HTTP status code and
+then write an error message to the response writer (w). The StatusNotFound vari-
+able is really just a constant representing the HTTP status code 404, and the
+WriteHeader method is one way to write HTTP status codes in Go.
+--  http.StatusNotFound is really just a constant that represents the 404 status
+code. This isn’t actually necessary, and you could replace it with 404 in your
+code, but constants like StatusNotFound are often exported by packages to make
+code easier to read and maintain.
+--  This is very hard to maintain with multiple pages. Instead break pages into
+their own function and use a router to call the function for the page.
 */
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email "+
+			"to <a href=\"mailto:support@lenslocked.com\">"+
+			"support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1> We could not find the page you "+
+			"were looking for.</h1>"+
+			"<p>Please email us if you keep being sent to an "+
+			"invalid page.</p>")
+	}
 }
 
 /*
