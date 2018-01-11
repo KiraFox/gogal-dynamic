4eb121b40d39da537e19d6df4a271cc0cff756db
Kira Fox <kira.fox@foxypossibilities.com>
2018-01-11 16:47:23 -0500

    Chapter 3.4 Add custom 404 page to gorilla/mux

--- main.go
+++ main.go
@@ -21,11 +21,24 @@ r.HandleFunc("/", home) & r.HandleFunc("/contact", contact)
 	+ Our router will in turn handle requests long enough to figure out which
 	function was assigned to that path, and then it will call that function.
 -- gorilla/mux provides a simple 404 status page by default
+	+ Custimize the 404 page by setting the NotFoundHandler attribute on the
+	gorilla/mux.Router
+	+ You need to initialize a variable as a http.Handler interface:
+	var nf http.Handler
+	+ Then set the variable (nf) to a http.HandlerFunc(...) with a function you
+	created for the 404 webpage:
+	nf = http.HandlerFunc(notFound)
+	+ Then set the NotFoundHandler on the gorilla/mux.Router to your variable:
+	r.NotFoundHandler = nf
 */
 func main() {
+	var nf http.Handler
+	nf = http.HandlerFunc(notFound)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = nf
 	http.ListenAndServe(":3000", r)
 }
 
@@ -41,4 +54,13 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email "+
 		"to <a href=\"mailto:support@lenslocked.com\">"+
 		"support@lenslocked.com</a>.")
+}
+
+// This is the function for a custom 404 status page.
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1> We could not find the page you "+
+		"were looking for.</h1>"+
+		"<p>Please email us if you keep being sent to an "+
+		"invalid page.</p>")
 }
