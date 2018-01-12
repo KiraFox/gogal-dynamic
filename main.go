e866cc91a57d6c00e21f25894b7da39d8c8756fb
Kira Fox <kira.fox@foxypossibilities.com>
2018-01-12 11:09:11 -0500

    Ch 6.1 Add first View and Template

--- main.go
+++ main.go
@@ -2,36 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-/*
-Routing via Gorilla Mux:
+// Using MVC : Create first View and Template
+// Create global variable to store the parsed template for home page instead of
+// using Fprint in the "home" func. Will be cleaned up later.
+var homeTemplate *template.Template
 
--- Have a function for each path(webpage).
--- First we create a new router: r := mux.NewRouter()
--- Then we start assigning functions to handle different paths:
-r.HandleFunc("/", home) & r.HandleFunc("/contact", contact)
--- Finally we start up our server: http.ListenAndServe(":3000", r)
-	+ This time we are passing in our router (r) as the default handler for web
-	requests instead of "nil" like before. This tells the ListenAndServe
-	function that we want to use our own custom router.
-	+ Our router will in turn handle requests long enough to figure out which
-	function was assigned to that path, and then it will call that function.
--- gorilla/mux provides a simple 404 status page by default
-	+ Custimize the 404 page by setting the NotFoundHandler attribute on the
-	gorilla/mux.Router
-	+ You need to initialize a variable as a http.Handler interface:
-	var nf http.Handler
-	+ Then set the variable (nf) to a http.HandlerFunc(...) with a function you
-	created for the 404 webpage:
-	nf = http.HandlerFunc(notFound)
-	+ Then set the NotFoundHandler on the gorilla/mux.Router to your variable:
-	r.NotFoundHandler = nf
-*/
 func main() {
+
+	var err error
+	// Make sure global variable is assigned to the parsed template
+	// Template paths are relative. Note that the file paths used to access them
+	// are relative to wherever you run your code.
+	// Parsing files can cause issues (return error) so check for errors
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	var nf http.Handler
 	nf = http.HandlerFunc(notFound)
 
@@ -45,7 +38,14 @@ func main() {
 // This is the function the router calls when a user visits the "home" page.
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	// Update function to use (Execute) new template (homeTemplate)
+	// We write the results to our Response Writer (w), and don't give any data
+	// to the template to use (data = nil) as the template doesn't currently
+	// use any dynamic data
+	// Executing templates can cause issues (return error) so check for errors
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 // This is the function the router calls when a user visits the "contact" page.
